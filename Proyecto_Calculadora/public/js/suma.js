1d4c27837ec6bf5986b041530e9cd5400a1b0542
gingerbanchon} <ggbanchon@espe.edu.ec>
2025-11-11 11:32:59 -0500

    Javascript multiplicacion - Ginger Banchon

---
+++ Proyecto_Calculadora/public/js/suma.js
@@ -0,0 +1,15 @@
+// capturar valores de caja de texto
+let num_uno = document.getElementById("num1");
+let num_dos = document.getElementById("num2");
+
+let btn_Sumar = document.getElementById("btnSumar");
+
+let resultado = document.getElementById("Resultado");
+
+let total_suma = 0;
+
+// Aplicar la operacion en el boton 
+btn_Sumar.addEventListener("click", function(evt) {
+    total_suma = parseFloat(num_uno.value) + parseFloat(num_dos.value);
+    resultado.value = total_suma;
+});
